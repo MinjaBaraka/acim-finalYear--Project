826d4fc0d40e32722ccc3b300e5ecc7cbcd47053
MinjaBaraka <45535420+MinjaBaraka@users.noreply.github.com>
2023-06-19 22:50:22 +0300

    Update searchcar_mechanics.dart

--- car_owner/lib/pages/admin/searchcar_mechanics.dart
+++ car_owner/lib/pages/admin/searchcar_mechanics.dart
@@ -12,37 +12,7 @@ class CarMechanicListScreen extends StatefulWidget {
 }
 
 class _CarMechanicListScreenState extends State<CarMechanicListScreen> {
-  // void fetchOnlineMechanics() {
-  //   DatabaseReference mechanicsRef =
-  //       FirebaseDatabase.instance.ref().child("acim_mechanics");
-
-  //   List<CarMechanic> onlineMechanics = [];
-
-  //   mechanicsRef
-  //       .orderByChild("newMechanicsServiceStatus")
-  //       .equalTo("Online")
-  //       .once()
-  //       .then((carMechanicsSnapshot) {
-  //     // print(carMechanicsSnapshot.snapshot.value);
-  //     if (carMechanicsSnapshot.snapshot.value != null) {
-  //       Map<String, dynamic> carMechanicsData =
-  //           carMechanicsSnapshot as Map<String, dynamic>;
-
-  //       carMechanicsData.forEach((key, value) {
-  //         CarMechanic carMechanic = CarMechanic(
-  //           key: '',
-  //           name: value["name"],
-  //           locationLatMechanics: double.parse(value["latitude"].toString()),
-  //           locationLngMechanics: double.parse(value["longitude"].toString()),
-  //         );
-  //         onlineMechanics.add(carMechanic);
-  //       });
-  //       print(onlineMechanics);
-  //     }
-  //     return onlineMechanics;
-  //   });
-  // }
-
+  
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -58,21 +28,6 @@ class _CarMechanicListScreenState extends State<CarMechanicListScreen> {
             ),
             elevation: 0,
           ),
-          // body: Column(
-          //   children: [
-          // const SizedBox(height: 20),
-          // Padding(
-          //   padding: const EdgeInsets.only(left: 80),
-          //   child: ElevatedButton.icon(
-          //     onPressed: () {
-          //       fetchOnlineMechanics();
-          //     },
-          //     icon: const Icon(Icons.list),
-          //     label: const Text("List Of Car Mechanics"),
-          //   ),
-          // ),
-          //   ],
-          // ),
           body: Container(
             decoration: BoxDecoration(
               color: Colors.grey,
@@ -88,9 +43,6 @@ class _CarMechanicListScreenState extends State<CarMechanicListScreen> {
                   Animation<double> animation, int index) {
                 Map carMechanicsSnapshot = snapshot.value as Map;
                 print(carMechanicsSnapshot);
-                // Process the snapshot data
-                // Map<String, dynamic> mechanicsData = snapshot.value;
-
                 return ListTile(
                   title: Text(carMechanicsSnapshot["name"]),
                   subtitle: Text(
